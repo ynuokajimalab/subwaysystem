a364aa0da3f4005020d1cb6c1ac8f0c33bc2378a
Takuya Daiyo <daiyo-takuya-yd@ynu.jp>
2017-03-03 02:46:24 +0900

    upload

--- subwaysystem/Source.cpp
+++ subwaysystem/Source.cpp
@@ -10,34 +10,39 @@
 #include "fir_filter.h"
 #include "fft.h"
 #include "countSound.h"
+#include "frame-time.h"
 
-#define N 500
+#define N 2000
 
 int main(void)
 {
 	MONO_PCM pcm0, pcm1;
-	int i, j, n, m, k, L, offset, frame, number_of_frame, number_fe1, number_fe2, count;
-	double fe1, fe2, *w, *x_real, *x_imag, *y_real, *y_imag, *w_real, *w_imag, max, max_total, max_average, threshold, temp, sum, sum_total, sum_average, sum_all, alpha, *A, *A_all, max_top;
+	int i, j, n, m, k, L, offset, frame, number_of_frame, number_fe1, number_fe2, count, n_noise1, n_noise2;
+	double fe1, fe2, *w, *x_real, *x_imag, *y_real, *y_imag, *w_real, *w_imag, *A, *A_all, *xw_real, *xw_imag, *xw;
+    double max, max_total, max_average, threshold, temp, sum, sum_total, sum_average, sum_all, alpha, max_top;
 	double noise_time1, noise_time2, number_noise1, number_noise2;
 	double data[N];
-	double data_all[N];
 
-	mono_wave_read(&pcm0, "30_29.wav"); /* WAVEファイルからモノラルの音データを入力する */
-
+	mono_wave_read(&pcm0,"ftk_yg_16bit.wav"); /* WAVEファイルからモノラルの音データを入力する */
+			
 	pcm1.fs = pcm0.fs; /* 標本化周波数 */
 	pcm1.bits = pcm0.bits; /* 量子化精度 */
 	pcm1.length = pcm0.length; /* 音データの長さ */
 	pcm1.s = (double*)calloc(pcm1.length, sizeof(double)); /* メモリの確保 */
 
+	for (int i = 0; i < pcm0.length; i++)
+	{
+		pcm1.s[i] = pcm0.s[i];
+	}
 	/* 周波数の範囲を指定 */
-	fe1 = 500.0 / pcm0.fs;
-	fe2 = 900.0 / pcm0.fs;
-	L = 256; /* フレームの長さ */
+	fe1 = 0.0 / pcm0.fs;
+	fe2 = 3000.0 / pcm0.fs;
+	L = 1024; /* フレームの長さ */
+	alpha = 40.0;
 
 	w = (double*)calloc(L, sizeof(double)); /* メモリの確保 */
 
 	Hanning_window(w, L); /* ハニング窓 */
-
 
 	number_of_frame = pcm0.length / L; /* フレームの数 */
 	printf("number_of_frame:%d\n", number_of_frame);
@@ -50,6 +55,9 @@ int main(void)
 	y_imag = (double*)calloc(L, sizeof(double)); /* メモリの確保 */
 	w_real = (double*)calloc(L, sizeof(double)); /* メモリの確保 */
 	w_imag = (double*)calloc(L, sizeof(double)); /* メモリの確保 */
+	xw_real = (double*)calloc(L, sizeof(double)); /* メモリの確保 */
+	xw_imag = (double*)calloc(L, sizeof(double)); /* メモリの確保 */
+	xw = (double*)calloc(L, sizeof(double)); /* メモリの確保 */
 	A = (double*)calloc(L, sizeof(double)); /* メモリの確保 */
 	A_all = (double*)calloc(L, sizeof(double)); /* メモリの確保 */
 
@@ -57,17 +65,17 @@ int main(void)
 	number_fe1 = (int)floor(L * fe1);
 	number_fe2 = (int)floor(L * fe2);
 
-	printf("number_fe1:%d     number_fe2:%d\n", number_fe1,number_fe2);
-
-	noise_time1 = 3.9;
-	noise_time2 = 4.1;
+	noise_time1 = 0.0;
+	noise_time2 = 0.2;
 	number_noise1 = noise_time1 * pcm0.fs /  L;
 	number_noise2 = noise_time2 * pcm0.fs /  L;
+	n_noise1 = noise_time1 * pcm0.fs;
+	n_noise2 = noise_time2 * pcm0.fs;
+	
 	sum = 0.0; /* 初期化 */
 	sum_total = 0.0;
-	alpha = 1.1;
 	max_total = 0.0;
-
+	
 	for (frame = number_noise1; frame < number_noise2 + 1; frame++)
 	{
 		offset = L * frame;
@@ -81,6 +89,7 @@ int main(void)
 		for (n = 0; n < L; n++)
 		{
 			x_real[n] = pcm0.s[offset + n];
+			
 		}
 
 		/* w(m)のFFT */
@@ -122,139 +131,19 @@ int main(void)
 			    }
 			}
 		}
-		for (i = number_fe1; i < (number_fe2 - number_fe1 + 1) / 4 + number_fe1 + 1; i++)
+		for (i = number_fe1; i < (number_fe2 - number_fe1) / 4 + number_fe1 + 1; i++)
 		{
 			sum += data[i];
 		}
 		IFFT(y_real, y_imag, L);
 
-		/* 最大値を利用した判定 */
-		/*for (i = number_fe1; i < number_fe2 + 1; i++)
-		{
-			data[i] = A[i];
-        }
-		max = y_real[number_fe1];
-		for (i = number_fe1; i < number_fe2 + 1; i++)
-		{
-			if (data[i] > max)
-			{
-				max = data[i];
-			}
-		}
-		max_top = max;
-		if (max > max_top)
-		{
-			max_top = max;
-		}*/
 	}
 	sum_total += sum;
 	sum_average = sum_total / (number_noise2 - number_noise1 + 1);
 	threshold = alpha * sum_average;
-
-	/*max_total += max;
-	max_average = max_total / (number_noise2 - number_noise1 + 1);
-	threshold = alpha * max_top;*/
-
-	for (frame = 0; frame < number_of_frame/2; frame++)
-	{
-		offset = L * frame;
-
-			/* x(n)のFFT */
-			for (n = 0; n < L; n++)
-			{
-				x_real[n] = 0.0;
-				x_imag[n] = 0.0;
-			}
-			for (n = 0; n < L; n++)
-			{
-				x_real[n] = pcm0.s[offset + n];
-			}
-			
-			/* w(m)のFFT */
-			for (m = 0; m < L; m++)
-			{
-				w_real[m] = 0.0;
-				w_imag[m] = 0.0;
-			}
-			for (m = 0; m < L; m++)
-			{
-				w_real[m] = w[m];
-			}
-			/*FFT(b_real, b_imag, N);*/
-
-			/* 掛け合わせ */
-			for (k = 0; k < L; k++)
-			{
-				y_real[k] = x_real[k] * w_real[k];
-				y_imag[k] = x_imag[k] * w_imag[k];
-			}
-			FFT(y_real, y_imag, L);
-
-			for (k = 0; k < L; k++)
-			{
-				A_all[k] = sqrt(y_real[k] * y_real[k] + y_imag[k] * y_imag[k]);
-			}
-
-			/* 上位1/4の合計を利用した判定 */
-			for (i = number_fe1; i < number_fe2 + 1; i++)
-			{
-				data_all[i] = A_all[i];
-				for (j = i + 1; j < number_fe2 + 1; j++)
-				{
-					if (data_all[i] < data_all[j])
-					{
-						temp = data_all[i];
-						data_all[i] = data_all[j];
-						data_all[j] = temp;
-					}
-				}
-			}
-
-			sum_all = 0; /* 初期化 */
-			for (i = number_fe1; i < (number_fe2 - number_fe1 + 1) / 4 + number_fe1 + 1; i++)
-			{
-				sum_all += data_all[i];
-			}
-
-			if (sum_all > threshold)
-			{
-				count++;
-				//?
-				printf("time:%f\n", (double)offset*2 /pcm0.fs);
-			}
-
-			/* 最大値を利用した判定 */
-			/*for (i = number_fe1; i < number_fe2 + 1; i++)
-			{
-				data_all[i] = A_all[i];
-			}
-
-			max = data_all[number_fe1];
-			for (i = number_fe1; i < number_fe2 + 1; i++)
-			{
-				if (data_all[i] > max)
-				{
-					max = data_all[i];
-				}
-			}
-
-			if (max > threshold)
-			{
-				count++;
-				printf("time:%f\n", (double)offset * 2 / pcm0.fs);
-			}*/
-
-		//		/* フィルタリング結果の連結 */
-		//		for (n = 0; n < L * 2; n++)
-		//		{
-		//			if (offset + n < pcm1.length)
-		//			{
-		//				pcm1.s[offset + n] += y_real[n];
-		//			}
-		//		}
-		//	}
-	}
-	printf("回数は%d回です\n", count);
+	printf("threshold:%f\n", threshold);
+	
+	CountTimes_mono(L, 0., 0.0, 0.0, number_fe1, number_fe2, threshold, 0.3, 2.5, &pcm0);
 
 	mono_wave_write(&pcm1, "test1.WAV"); /* WAVEファイルにモノラルの音データを出力する */
 
--- subwaysystem/countSound.h
+++ subwaysystem/countSound.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "wave.h"
 #include "dfr_filter.h"
+#include "frame-time.h"
+#include "statistics.h"
+#define M 2000
 
 int countsound(double sound[], int soundlength, int framelength, double threshold) {
 
@@ -11,5 +14,324 @@ int countsound(double sound[], int soundlength, int framelength, double threshol
 int judgeSounnd(double sound[], int N, double threshold) {
 
 	int count = 0;
+	return count;
+}
+
+int CountTimes(int N, double sleeptime, double s, double t, int number_fe1, int number_fe2, double threshold, double threshold_2, double threshold_3, STEREO_PCM* org_pcm)
+{
+	double temptime, counttime, sum_all, data_all[M], *A, *A_all, temp, *w_real, *w_imag, *x_imag, *x_real, *y_imag, *y_real, *w, *data_ex, myu, sd, data_sd[M];
+	int maxindex, minindex, representive_index, secondindex, bflag, cflag,sleepframe, count, frame, data, number_of_frame, i, j, k, m, n, offset, judge;
+	bflag = 0;	//前フレームで閾値を超えたか判定
+	cflag = 0;	//カウントをチェックするかどうか判定
+	counttime = 0.0;	//前のカウント時刻
+	count = 0;
+	w = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	Hanning_window(w, N); /* ハニング窓 */
+
+	x_real = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	x_imag = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	y_real = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	y_imag = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	w_real = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	w_imag = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	A = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	A_all = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	number_of_frame = org_pcm->length / N;
+	for (frame = 0; frame < number_of_frame; frame++)
+	{
+		offset = (N / 2) * frame;
+
+		int maxindex, minindex, index;
+		double max, min, data_ex, delta;
+
+		for (n = 0; n < N; n++)
+		{
+			x_real[n] = 0.0;
+			x_imag[n] = 0.0;
+		}
+		for (n = 0; n < N / 2; n++)
+		{
+			x_real[2 * n] = org_pcm->sL[offset + n];
+			x_real[2 * n + 1] = org_pcm->sR[offset + n];
+		}
+		
+		max = -1.0;
+       	min = 1.0;
+		
+
+			for (i = 0; i < N; i++) 
+			{
+				data_ex = x_real[i];
+				if (max < data_ex) {
+					max = data_ex;
+					maxindex = i;
+				}
+				if (min > data_ex) {
+					min = data_ex;
+					minindex = i;
+				}
+				
+			}
+			
+
+		/*delta = max - min;*/
+		if (max > -min) {
+			delta = max;
+			index = maxindex;
+		}
+		else {
+			delta = -min;
+			index = minindex;
+		}
+
+		if (delta > threshold_2) {
+			judge = 1;
+		}
+		else{
+			judge = 0;
+		}
+		/*printf("delta = %f\n", delta);*/
+		
+
+		/* w(m)のFFT */
+		for (m = 0; m < N; m++)
+		{
+			w_real[m] = 0.0;
+			w_imag[m] = 0.0;
+		}
+		for (m = 0; m < N; m++)
+		{
+			w_real[m] = w[m];
+		}
+		/*FFT(b_real, b_imag, N);*/
+
+		/* 掛け合わせ */
+		for (k = 0; k < N; k++)
+		{
+			y_real[k] = x_real[k] * w_real[k];
+			y_imag[k] = x_imag[k] * w_imag[k];
+		}
+		FFT(y_real, y_imag, N);
+
+
+		double A_all[M];
+		for (k = 0; k < N; k++)
+		{
+			A_all[k] = sqrt(y_real[k] * y_real[k] + y_imag[k] * y_imag[k]);
+		}
+
+		/* 上位1/4の合計を利用した判定 */
+		for (i = number_fe1; i < number_fe2 + 1; i++)
+		{
+			data_all[i] = A_all[i];
+			for (j = i + 1; j < number_fe2 + 1; j++)
+			{
+				if (data_all[i] < data_all[j])
+				{
+					temp = data_all[i];
+					data_all[i] = data_all[j];
+					data_all[j] = temp;
+				}
+			}
+		}
+
+		sum_all = 0; /* 初期化 */
+			for (i = number_fe1; i < (number_fe2 - number_fe1) / 4 + number_fe1 + 1; i++)
+			{
+				sum_all += data_all[i];
+			}
+
+			for (i = 0; i < N; i++)
+			{
+				data_sd[i] = A_all[i];
+			}
+			myu = getsoundmyu(data_sd, N);
+			sd = getSD(data_sd, myu, N);
+
+		if (cflag == 0) {
+			
+			if ((sum_all > threshold) && (judge == 1) && (sd<threshold_3)) {
+				//printf("over:%lf[s]\n",getsecond(frame,L,org_pcm.fs));
+				count++;
+				temptime = getsecond(frame, N, org_pcm->fs);
+				if (sleeptime > (temptime - counttime)*t) {
+					sleeptime -= (temptime - counttime) * s;
+				}
+				else if (sleeptime < (temptime - counttime)*t) {
+					sleeptime += (temptime - counttime) * s;
+				}
+				counttime = temptime;
+				sleepframe = getframe(sleeptime, N, org_pcm->fs);
+				/*printf("frame = %d, time = %lf, count = %d\n", frame - 1, counttime, count);
+				printf("カウント休止時間：%lf[s]\nカウント休止フレーム数：%d\n", sleeptime, sleepframe);*/
+				printf("time:%f    count = %d\n    delta = %f\n", (double)N * frame  / org_pcm->fs, count, delta);
+				cflag = sleepframe;
+				//bflag = 1;
+			}
+		}
+		else {
+			cflag--;
+		}	
+	}
+	printf("回数は%d回です\n", count);
+	return count;
+}
+
+int CountTimes_mono(int N, double sleeptime, double s, double t, int number_fe1, int number_fe2, double threshold, double threshold_2, double threshold_3, MONO_PCM* org_pcm)
+{
+	double temptime, counttime, sum_all, data_all[M], *A, *A_all, temp, *w_real, *w_imag, *x_imag, *x_real, *y_imag, *y_real, *w, *data_ex, data_sd[M], myu, sd;
+	int maxindex, minindex, representive_index, secondindex, bflag, cflag, sleepframe, count, frame, data, number_of_frame, i, j, k, m, n, offset, judge;
+	bflag = 0;	//前フレームで閾値を超えたか判定
+	cflag = 0;	//カウントをチェックするかどうか判定
+	counttime = 0.0;	//前のカウント時刻
+	count = 0;
+	w = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	Hanning_window(w, N); /* ハニング窓 */
+
+	x_real = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	x_imag = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	y_real = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	y_imag = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	w_real = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	w_imag = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	A = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	A_all = (double*)calloc(N, sizeof(double)); /* メモリの確保 */
+	number_of_frame = org_pcm->length / N;
+	for (frame = 0; frame < number_of_frame; frame++)
+	{
+		offset = N * frame;
+
+		int maxindex, minindex, index;
+		double max, min, data_ex, delta;
+
+		for (n = 0; n < N; n++)
+		{
+			x_real[n] = 0.0;
+			x_imag[n] = 0.0;
+		}
+		for (n = 0; n < N; n++)
+		{
+			x_real[n] = org_pcm->s[offset + n];
+		}
+
+		max = -1.0;
+		min = 1.0;
+
+
+		for (i = 0; i < N; i++)
+		{
+			data_ex = x_real[i];
+			if (max < data_ex) {
+				max = data_ex;
+				maxindex = i;
+			}
+			if (min > data_ex) {
+				min = data_ex;
+				minindex = i;
+			}
+
+		}
+
+
+		/*delta = max - min;*/
+		if (max > -min) {
+			delta = max;
+			index = maxindex;
+		}
+		else {
+			delta = -min;
+			index = minindex;
+		}
+
+		if (delta > threshold_2) {
+			judge = 1;
+		}
+		else {
+			judge = 0;
+		}
+		/*printf("delta = %f\n", delta);*/
+
+
+		/* w(m)のFFT */
+		for (m = 0; m < N; m++)
+		{
+			w_real[m] = 0.0;
+			w_imag[m] = 0.0;
+		}
+		for (m = 0; m < N; m++)
+		{
+			w_real[m] = w[m];
+		}
+		/*FFT(b_real, b_imag, N);*/
+
+		/* 掛け合わせ */
+		for (k = 0; k < N; k++)
+		{
+			y_real[k] = x_real[k] * w_real[k];
+			y_imag[k] = x_imag[k] * w_imag[k];
+		}
+		FFT(y_real, y_imag, N);
+
+
+		double A_all[M];
+		for (k = 0; k < N; k++)
+		{
+			A_all[k] = sqrt(y_real[k] * y_real[k] + y_imag[k] * y_imag[k]);
+		}
+
+		/* 上位1/4の合計を利用した判定 */
+		for (i = number_fe1; i < number_fe2 + 1; i++)
+		{
+			data_all[i] = A_all[i];
+			for (j = i + 1; j < number_fe2 + 1; j++)
+			{
+				if (data_all[i] < data_all[j])
+				{
+					temp = data_all[i];
+					data_all[i] = data_all[j];
+					data_all[j] = temp;
+				}
+			}
+		}
+
+		sum_all = 0; /* 初期化 */
+		for (i = number_fe1; i < (number_fe2 - number_fe1) / 4 + number_fe1 + 1; i++)
+		{
+			sum_all += data_all[i];
+		}
+
+		for (i = 0; i < N; i++)
+		{
+			data_sd[i] = A_all[i];
+		}
+		myu = getsoundmyu(data_sd, N);
+		sd = getSD(data_sd, myu, N);
+
+		if (cflag == 0) {
+
+			if ((sum_all>threshold) && (judge == 1) && (sd < threshold_3)) {
+				//printf("over:%lf[s]\n",getsecond(frame,L,org_pcm.fs));
+				count++;
+				temptime = getsecond(frame, N, org_pcm->fs);
+				if (sleeptime > (temptime - counttime)*t) {
+					sleeptime -= (temptime - counttime) * s;
+				}
+				else if (sleeptime < (temptime - counttime)*t) {
+					sleeptime += (temptime - counttime) * s;
+				}
+				counttime = temptime;
+				sleepframe = getframe(sleeptime, N, org_pcm->fs);
+				/*printf("frame = %d, time = %lf, count = %d\n", frame - 1, counttime, count);
+				printf("カウント休止時間：%lf[s]\nカウント休止フレーム数：%d\n", sleeptime, sleepframe);*/
+				printf("time:%f    count = %d\n    標準偏差: %f\n", (double)N * frame / org_pcm->fs, count, sd);
+				cflag = sleepframe;
+				//bflag = 1;
+			}
+		}
+		else {
+			cflag--;
+		}
+	}
+	printf("回数は%d回です\n", count);
 	return count;
 }
